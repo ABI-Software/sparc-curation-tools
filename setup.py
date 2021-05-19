73fe2bdffdea02781b4105db47768439bad248ae
hsorby <h.sorby@auckland.ac.nz>
2021-05-19 12:30:43 +1200

    Add entry points for scripts.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requires = ['pandas', 'openpyxl']
 
 setup(
     name='sparc-curation-tools',
-    version='1.0.0',
+    version='0.1.0',
     description='A collection of tools to help with curating SPARC datasets..',
     long_description='\n'.join(readme) + software_licence,
     long_description_content_type='text/x-rst',
@@ -36,6 +36,12 @@ setup(
     license_files=("LICENSE",),
     packages=find_packages("src"),
     package_dir={"": "src"},
+    entry_points={
+        "console_scripts": [
+            "scaffold-annotations = scaffold_annotations:main",
+            "plot-annotation = plot_annotation:main"
+        ]
+    },
     include_package_data=True,
     zip_safe=False,
     install_requires=requires,
